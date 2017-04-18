874956c929708ad06f7c65be199ffb45e81423b7
Philipp Oppermann <dev@phil-opp.com>
2017-04-18 13:39:31 +0200

    Implement allocation functions for bump allocator

--- libs/bump_allocator/src/lib.rs
+++ libs/bump_allocator/src/lib.rs
@@ -1,4 +1,18 @@
+#![feature(allocator)]
 #![feature(const_fn)]
+
+#![allocator]
+#![no_std]
+
+use spin::Mutex;
+
+extern crate spin;
+
+pub const HEAP_START: usize = 0o_000_001_000_000_0000;
+pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+
+static BUMP_ALLOCATOR: Mutex<BumpAllocator> = Mutex::new(
+    BumpAllocator::new(HEAP_START, HEAP_SIZE));
 
 #[derive(Debug)]
 struct BumpAllocator {
@@ -48,4 +62,43 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 /// so that x >= addr. The alignment must be a power of 2.
 pub fn align_up(addr: usize, align: usize) -> usize {
     align_down(addr + align - 1, align)
+}
+
+#[no_mangle]
+pub extern fn __rust_allocate(size: usize, align: usize) -> *mut u8 {
+    BUMP_ALLOCATOR.lock().allocate(size, align).expect("out of memory")
+}
+
+#[no_mangle]
+pub extern fn __rust_deallocate(_ptr: *mut u8, _size: usize,
+    _align: usize)
+{
+    // just leak it
+}
+
+#[no_mangle]
+pub extern fn __rust_usable_size(size: usize, _align: usize) -> usize {
+    size
+}
+
+#[no_mangle]
+pub extern fn __rust_reallocate_inplace(_ptr: *mut u8, size: usize,
+    _new_size: usize, _align: usize) -> usize
+{
+    size
+}
+
+#[no_mangle]
+pub extern fn __rust_reallocate(ptr: *mut u8, size: usize, new_size: usize,
+                                align: usize) -> *mut u8 {
+    use core::{ptr, cmp};
+
+    // from: https://github.com/rust-lang/rust/blob/
+    //     c66d2380a810c9a2b3dbb4f93a830b101ee49cc2/
+    //     src/liballoc_system/lib.rs#L98-L101
+
+    let new_ptr = __rust_allocate(new_size, align);
+    unsafe { ptr::copy(ptr, new_ptr, cmp::min(size, new_size)) };
+    __rust_deallocate(ptr, size, align);
+    new_ptr
 }
